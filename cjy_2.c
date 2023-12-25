d99f48ba783c125f6a0a3bd57ea1996316153d0d
CJYXB <3049464238@qq.com>
2023-12-25 18:16:04 +0800

    cjy_2

---
+++ cjy_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("*********************************\n\n");
+	printf("\t1------ADD(+)\n");
+	printf("\t2------SUB(-)\n");
+	printf("\t3------MUL(*)\n");
+	printf("\t4------DIV(/)");
+	printf("\n\n*********************************");
+	return 0;
+} 
